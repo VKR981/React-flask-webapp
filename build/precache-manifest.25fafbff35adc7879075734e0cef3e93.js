edb984b115b52e97994017145d22bea94ca320b9
VKR981 <66425536+VKR981@users.noreply.github.com>
2020-06-30 00:49:34 +0530

    added build folder

---
+++ build/precache-manifest.25fafbff35adc7879075734e0cef3e93.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d975facb09bad9f9d01ebebb5aafdb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16e1f783c43f1eb1fe7d",
+    "url": "/static/css/main.93c0bff3.chunk.css"
+  },
+  {
+    "revision": "73d2c51aee1f317f2a2c",
+    "url": "/static/js/2.2a4d83ae.chunk.js"
+  },
+  {
+    "revision": "2089965cc3e0fbb1dda2d650d8277df4",
+    "url": "/static/js/2.2a4d83ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e1f783c43f1eb1fe7d",
+    "url": "/static/js/main.ef466994.chunk.js"
+  },
+  {
+    "revision": "439be538527d74fd1d4c",
+    "url": "/static/js/runtime-main.6de041b3.js"
+  },
+  {
+    "revision": "0590bbb0f3a5b8fbbbc1d05349972ddf",
+    "url": "/static/media/book.0590bbb0.jpg"
+  },
+  {
+    "revision": "4f9cf305bf4fedcfdec0ef4b8767b299",
+    "url": "/static/media/book.4f9cf305.jpg"
+  },
+  {
+    "revision": "b710acbd756acfb7849c84d6333c7807",
+    "url": "/static/media/loading.b710acbd.gif"
+  },
+  {
+    "revision": "64d6eaa371dad173979b8acc7e6204d2",
+    "url": "/static/media/pp.64d6eaa3.png"
+  }
+]);
